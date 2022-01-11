745b0d9b042b837ec6b3367eaf60bffa44721e97
Tatang Romadhona <itsme@tatang.dev>
2022-01-11 17:25:50 +0700

    add responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'dm-serif': "'DM Serif Display', serif",
+        'fira-code': "'Fira Code', monospace",
+        poppins: "'Poppins', sans-serif"
+      }
+    },
   },
+
   plugins: [],
 }
